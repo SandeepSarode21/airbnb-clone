3a64e0241227946ac0dfa9e22818beae60d7f8bb
Sandeep Sarode <sandeepsarode21@gmail.com>
2021-10-25 07:04:08 -0400

    Added largecard and footer component

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Banner from "../components/Banner";
-import SmallCards from "../components/SmallCards";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard";
+import LargeCard from "../components/LargeCard";
+import Footer from "../components/Footer";
 
-export default function Home({ exploreData }) {
+export default function Home({ exploreData, cardsData }) {
   return (
     <div className="">
       <Head>
@@ -20,8 +23,8 @@ export default function Home({ exploreData }) {
 
           {/* Pull Data from api */}
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-            {exploreData.map((item) => (
-              <SmallCards
+            {exploreData?.map((item) => (
+              <SmallCard
                 key={item.img}
                 img={item.img}
                 location={item.location}
@@ -30,7 +33,22 @@ export default function Home({ exploreData }) {
             ))}
           </div>
         </section>
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+          <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3">
+            {cardsData?.map((item) => (
+              <MediumCard key={item.img} img={item.img} title={item.title} />
+            ))}
+          </div>
+        </section>
+        <LargeCard
+          img="https://links.papareact.com/4cj"
+          title="The Greatest Outdoors"
+          description="Wishlist curated by Airbnb."
+          buttonText="Get Insprired"
+        />
       </main>
+      <Footer />
     </div>
   );
 }
@@ -40,9 +58,14 @@ export async function getStaticProps() {
     (res) => res.json()
   );
 
+  const cardsData = await fetch("https://links.papareact.com/zp1").then((res) =>
+    res.json()
+  );
+
   return {
     props: {
       exploreData,
+      cardsData,
     },
   };
 }
