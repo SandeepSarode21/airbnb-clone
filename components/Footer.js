e3e53017c405293cfd48bce30a5e939e3029924e
Sandeep Sarode <sandeepsarode21@gmail.com>
2021-10-25 07:28:40 -0400

    Added footer component

--- components/Footer.js
+++ components/Footer.js
@@ -1,5 +1,40 @@
 function Footer() {
-  return <div></div>;
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100">
+      <div className="space-y-4 text-xs  text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investor</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accesibility</p>
+        <p>This is not a real site</p>
+        <p>Its pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Ninja Coder</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Like my clone</p>
+        <p>Follow me on github</p>
+        <p>Airbnb clone using Nextjs</p>
+        <p>Airbnb clone using Nextjs</p>
+        <p>Hit star</p>
+      </div>
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>Help center </p>
+        <p>Trust & Saftey</p>
+        <p>Hello github</p>
+        <p>Hallowen</p>
+        <p>Thankyou</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
