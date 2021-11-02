ae594e6da2b55a5361b74023057cc2e667e12af7
Sandeep Sarode <sandeepsarode21@gmail.com>
2021-11-02 07:28:27 -0400

    Added progress bar and updated info cards

--- components/InfoCard.js
+++ components/InfoCard.js
@@ -13,14 +13,32 @@ function InfoCard({
   lat,
 }) {
   return (
-    <div>
+    <div className="flex py-7 px-2 border-b cursor-pointer hover:opacity-80 hover:shadow-lg pr-4 transition-transform duration-200 ease-out first:border-t">
       <div className="relative h-24 w-40 md:h-52 md:w-80 flex-shrink-0">
-        <Image src={img} layout="fill" objectFit="cover" />
+        <Image
+          src={img}
+          layout="fill"
+          objectFit="cover"
+          className="rounded-2xl"
+        />
       </div>
-      <div>
+      <div className="flex-col flex-grow pl-5">
         <div className="flex justify-between">
           <p>{location}</p>
           <HeartIcon className="h-7 cursor-pointer" />
+        </div>
+        <h4 className="text-xl">{title}</h4>
+        <div className="border-b w-10 pt-2"></div>
+        <p className="pt-2 text-sm text-gray-500 flex-grow">{description}</p>
+        <div className="flex justify-between items-end pt-10">
+          <p className="flex items-center">
+            <StarIcon className="h-5 text-red-400" />
+            {star}
+          </p>
+          <div>
+            <p className="text-lg font-semibold pb-2 lg:text-2xl">{price}</p>
+            <p className="text-right font-extralight ">{total}</p>
+          </div>
         </div>
       </div>
     </div>
